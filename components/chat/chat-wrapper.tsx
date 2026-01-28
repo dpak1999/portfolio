ce31f84a4a541caf8555a8a42330dc69ba0f9232
Deepak Kumar <deepakkumardash313@gmail.com>
2026-01-28 21:33:51 +0530

    feat: Introduce chat component, fetching profile data from Sanity, and update related types

--- components/chat/chat-wrapper.tsx
+++ components/chat/chat-wrapper.tsx
@@ -1,5 +1,34 @@
-const ChatWrapper = () => {
-  return <div>ChatWrapper</div>;
+import { sanityFetch } from "@/sanity/lib/live";
+import { defineQuery } from "next-sanity";
+import Chat from "./chat";
+
+const CHAT_PROFILE_QUERY = defineQuery(`*[_id == "singleton-profile"][0]{
+    _id,
+    _type,
+    _createdAt,
+    _updatedAt,
+    _rev,
+    firstName,
+    lastName,
+    headline,
+    shortBio,
+    email,
+    phone,
+    location,
+    availability,
+    socialLinks,
+    yearsOfExperience,
+    profileImage
+  }`);
+
+const ChatWrapper = async () => {
+  const { data: profile } = await sanityFetch({ query: CHAT_PROFILE_QUERY });
+
+  return (
+    <div className="h-full w-full">
+      <Chat profile={profile} />
+    </div>
+  );
 };
 
 export default ChatWrapper;
